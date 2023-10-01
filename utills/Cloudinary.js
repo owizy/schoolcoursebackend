cd5bfde687dd4830ce54492908ced6774c395953
owizy <128070074+owizy@users.noreply.github.com>
2023-10-01 07:47:08 +0100

    Add files via upload

---
+++ utills/Cloudinary.js
@@ -0,0 +1,11 @@
+import { v2 as cloudinary } from 'cloudinary';
+cloudinary.config({
+    cloud_name: "dfklwscdp",
+    api_key:"727574948559188",
+    api_secret: "COkuWz_DzsQNHXQCloz6YPyjO1k",
+    secure: true
+  });
+
+  export default cloudinary;
+
+  
